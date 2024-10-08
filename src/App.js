303ea9ec2ada7f2dba59dbb506b630b1786006e3
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2024-10-08 01:21:48 -0300

    Siempre te recordare gordito hermoso

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // src/App.js
-import React from 'react';
-import HomePage from './pages/HomePage';
+import React from "react";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <Footer />
+      <ScrollToTop />
     </div>
   );
 }
---
+++ src/components/scrollToTop.js
@@ -0,0 +1,44 @@
+// src/components/ScrollToTop.js
+import React, { useState, useEffect } from 'react';
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  return (
+    <>
+      {isVisible && (
+        <div
+          onClick={scrollToTop}
+          className="fixed bottom-5 right-5 cursor-pointer bg-indigo-600 text-white p-2 rounded-full shadow-lg hover:bg-indigo-700 transition-colors duration-300"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" />
+          </svg>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ScrollToTop;

2f2d79d4f57db1f35cd06a3bb5b213320040f32a
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2024-10-08 01:24:47 -0300

    Finalizado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // src/App.js
 import React from "react";
 import HomePage from "./pages/HomePage";
+import Footer from './components/Footer'; // Importa el nuevo componente Footer
+import ScrollToTop from './components/ScrollToTop'; // Importa el nuevo componente ScrollToTop
 
 function App() {
   return (
